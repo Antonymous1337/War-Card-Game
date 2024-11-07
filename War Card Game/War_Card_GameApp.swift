7bf046427742d237ded17da39a1bb296f24242d7
Antonymous1337 <holshouserantony@yahoo.com>
2024-11-07 11:29:06 -0800

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Antony Holshouser on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
